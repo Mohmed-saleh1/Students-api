2ff93597be00534079740aaf88f3200a0cdb5e53
Mu saleh <mohamed.s.kamaal@gmail.com>
2023-10-15 22:48:02 -0700

    code refactoring of updatestudent controller

--- src/students/controller.js
+++ src/students/controller.js
@@ -80,22 +80,21 @@ const deleteStudentByID = (req,res) => {
  * @method put
  * @route api/v1/students/id
  */
-const updateStudent = (req,res) => {
-  const {id} = +req.params
-  const {name,age}= req.body
-        pool.query(getStudentByIdQuery,[id],(error,results) => {
-        if (error) throw error
-        if (!results.rows.length) {
-            res.send("there is no students for this id")
-        }
-
-        pool.query(updateStudentQuery,[name,age,id],(error,results) => {
-        if (error) throw error
-        res.status(200).json('A student updated')
-        })
+const updateStudent = (req, res) => {
+    const id = parseInt(req.params.id)
+    const {name} = req.body
+    
+    pool.query(getStudentByIdQuery,[id],(error,results) => {
+      if (!results.rows.length) {
+        res.status(400).json(`there is no students for this id`)
+      }
+      pool.query(updateStudentQuery,[name,id],(error,results) => {
+        if(error) throw error;
+        res.status(200).json(`the student for id ${id} updated success`)
       })
-  
-    }
+    })
+}
+
  
   module.exports={
     getAllStudents,
--- src/students/queries.js
+++ src/students/queries.js
@@ -3,11 +3,12 @@ const getStudentByIdQuery =  "SELECT * FROM students WHERE id = $1";
 const checkEmailExists =  "SELECT s FROM students s WHERE s.email = $1";
 const addStudentQuery = "INSERT INTO students (name,email,age,dob) VALUES ($1,$2,$3,$4)";
 const deleteStudentQuery = "DELETE FROM students WHERE id = $1";
-const updateStudentQuery = "UPDATE students SET name = $1 , age = $2 WHERE id =$3"
+const updateStudentQuery = "UPDATE students SET name = $1  WHERE id =$2";
 module.exports={
     getAllStudentsQuery,
     getStudentByIdQuery,
     addStudentQuery,
     checkEmailExists,
     deleteStudentQuery,
+    updateStudentQuery
 }
